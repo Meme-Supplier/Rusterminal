b5086413534cf72d6fc44bb755446ee4b7688a99
Meme Guy <owen.debiasio@gmail.com>
2025-05-02 17:04:57 -0400

    Update upgrade.sh

--- src/upgrade.sh
+++ src/upgrade.sh
@@ -5,27 +5,16 @@ echo "Upgrading Rusterminal..."
 
 cd ~/
 
-
 if [ -d ~/rusterminal ]; then
     sudo rm -rf ~/rusterminal/
 fi
 
 git clone https://github.com/Meme-Supplier/Rusterminal.git
 
-cd Rusterminal
-cd src
+cd Rusterminal/src
 
 sudo chmod +x install.sh
 
 ./install.sh
 
-source ~/.bashrc
-
 echo "Rusterminal successfully updated!"
-
-echo -e "Do you want to launch Rusterminal?\n(Y or N)"
-read -r answer
-
-case "$answer" in
-    [Yy]) rusterminal ;;
-esac
