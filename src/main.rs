2cfef4940a2108c1122ced5e55c94d3ebb212daa
Meme Supplier <owen.debiasio@gmail.com>
2025-05-30 15:44:29 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 #!/usr/bin/env rust-script
 #[cfg(target_os = "linux")]
 
-/* 2025 Meme Supplier
-memesupplierbusiness@gmail.com
-Maintained by Meme Supplier */
-
 use hostname::get;
 use rustyline::error::ReadlineError;
 use rustyline::{Config, DefaultEditor};
