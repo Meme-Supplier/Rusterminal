3fc5f407db34a0352618267c70ff6d68a4a42e7c
Meme Supplier <owen.debiasio@gmail.com>
2025-05-31 10:59:58 -0400

    Update logger.rs

--- src/logger.rs
+++ src/logger.rs
@@ -1,6 +1,5 @@
 #!/usr/bin/env rust-script
 #[cfg(target_os = "linux")]
-
 use chrono::Local;
 use std::env;
 use std::fs::OpenOptions;
@@ -12,7 +11,6 @@ fn get_log_path() -> std::io::Result<PathBuf> {
         env::var("HOME").map_err(|e| std::io::Error::new(std::io::ErrorKind::NotFound, e))?;
     let mut path = PathBuf::from(home);
     path.push("rusterminal/log.txt");
-    log(&format!("logger::get_log_path(): Home path: {}", &path.to_str().unwrap()));
     Ok(path)
 }
 
@@ -20,7 +18,6 @@ fn write_to_file(text: &str) -> std::io::Result<()> {
     let path = get_log_path()?;
     let mut file = OpenOptions::new().create(true).append(true).open(path)?;
     writeln!(file, "{text}")?;
-    log(&format!("logger::write_to_file(): Wrote to file: {text}"));
     Ok(())
 }
 
@@ -32,13 +29,11 @@ pub fn log(text: &str) {
     let time = get_time();
     if let Err(e) = write_to_file(&format!("{time}: {text}")) {
         eprintln!("Failed to write log: {e}");
-        log(&format!("logger::log(): Failed to write log: {e}"))
     }
 }
 
 pub fn init(init: &str) {
     if let Err(e) = write_to_file(init) {
         eprintln!("Failed to init log: {e}");
-        log(&format!("logger::init(): Failed to init log: {e}"))
     }
 }
