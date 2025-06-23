3ce9a8422db243b031813372ed2c9238d0f9d940
Meme Supplier <owen.debiasio@gmail.com>
2025-06-23 16:53:15 -0400

    Update logger.rs

--- src/logger.rs
+++ src/logger.rs
@@ -4,36 +4,36 @@ use chrono::Local;
 use std::env;
 use std::fs::OpenOptions;
 use std::io::Write;
-use std::path::PathBuf;
 
-fn get_log_path() -> std::io::Result<PathBuf> {
-    let home =
-        env::var("HOME").map_err(|e| std::io::Error::new(std::io::ErrorKind::NotFound, e))?;
-    let mut path = PathBuf::from(home);
-    path.push("rusterminal/log.txt");
-    Ok(path)
-}
-
-fn write_to_file(text: &str) -> std::io::Result<()> {
-    let path = get_log_path()?;
-    let mut file = OpenOptions::new().create(true).append(true).open(path)?;
-    writeln!(file, "{text}")?;
-    Ok(())
+pub fn get_home() -> String {
+    env::var("HOME").expect("Failed to get HOME directory")
 }
 
 pub fn get_time() -> String {
     Local::now().format("%Y-%m-%d %H:%M:%S").to_string()
 }
 
+pub fn write_to_file(text: &str, path: &str) -> std::io::Result<()> {
+    let mut file = OpenOptions::new().create(true).append(true).open(path)?;
+    writeln!(file, "{text}")?;
+    Ok(())
+}
+
 pub fn log(text: &str) {
     let time = get_time();
-    if let Err(e) = write_to_file(&format!("{time}: {text}")) {
+    let home = get_home();
+
+    if let Err(e) = write_to_file(
+        &format!("{time}: {text}"),
+        &format!("{home}/rusterminal/log.txt"),
+    ) {
         eprintln!("Failed to write log: {e}");
     }
 }
 
 pub fn init(init: &str) {
-    if let Err(e) = write_to_file(init) {
+    let home = get_home();
+    if let Err(e) = write_to_file(init, &format!("{home}/rusterminal/log.txt")) {
         eprintln!("Failed to init log: {e}");
     }
 }
