2628c742b69f9e652cfca17f69e762ee8084de97
Meme Supplier <owen.debiasio@gmail.com>
2025-06-27 16:35:21 -0400

    Update logger.rs

--- src/logger.rs
+++ src/logger.rs
@@ -5,12 +5,30 @@ use std::env;
 use std::fs::OpenOptions;
 use std::io::Write;
 
+use crate::funcs;
+
 pub fn get_home() -> String {
     env::var("HOME").expect("Failed to get HOME directory")
 }
 
+pub fn get_time_format() -> String {
+    let config = &funcs::load_configs();
+
+    let format = &config
+        .get("logTimeAndDateFormat")
+        .map(|s| s.as_str())
+        .unwrap_or_default()[1..config
+        .get("logTimeAndDateFormat")
+        .map(|s| s.as_str())
+        .unwrap_or_default()[1..]
+        .len()];
+
+    format.to_string()
+}
+
 pub fn get_time() -> String {
-    Local::now().format("%Y-%m-%d %H:%M:%S").to_string()
+    let format = &get_time_format();
+    Local::now().format(format).to_string()
 }
 
 pub fn write_to_file(text: &str, path: &str) -> std::io::Result<()> {
