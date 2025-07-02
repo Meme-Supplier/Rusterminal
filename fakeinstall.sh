a1aee3725635cb524a41dd689a4c9e441c0f193e
Meme Supplier <owen.debiasio@gmail.com>
2025-07-02 16:24:20 -0400

    Update fakeinstall.sh

--- fakeinstall.sh
+++ fakeinstall.sh
@@ -7,7 +7,10 @@
 # For developers only
 # Replace path names as needed
 
-rm -r ~/rusterminal
+if [ -d ~/rusterminal ]; then
+    sudo rm -rf ~/rusterminal/
+fi
+
 cp -r ~/Documents/Code/Rust/Rusterminal/ ~/
 cd ~/Rusterminal/installer || exit
 bash install.sh
